5564027e16120501fabb3dc16d5d5f098dc4365f
frontdevuz <davlatovizzatillo7@gmail.com>
2025-12-25 21:57:59 +0500

    I changed my project

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <h1>I started my project in Vite + React at 9:56 PM 25/12/2025</h1>
   </StrictMode>,
 )
